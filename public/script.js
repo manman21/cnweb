7abdb646daf00581a8e14398807f444978372483
manman21 <manhnt62@wru.vn>
2020-07-03 17:59:47 +0700

    ngay 3/7

--- public/script.js
+++ public/script.js
@@ -1,33 +1,51 @@
 $(document).ready(function(){
 
   $(".nav-item").mouseenter(function(){
-    $(".submenu",this).toggleClass("hideShowCase")
+   	 $(".submenu",this).toggleClass("hideShowCase")
   });
 
   $(".nav-item").mouseleave(function(){
-    $(".submenu",this).toggleClass("hideShowCase")
+   	 $(".submenu",this).toggleClass("hideShowCase")
   });
+
   var x = 970;
   $(window).resize(function(){
-    if($(window).width() <= x){
-    	$("#sidenavid").removeClass("hideShowCase");
-    }
-    else{	
-    	$("#sidenavid").addClass("hideShowCase");
-    }
+	    if($(window).width() <= x){
+	    	$("#sidenavid").removeClass("hideShowCase");
+	    }
+	    else{	
+	    	$("#sidenavid").addClass("hideShowCase");
+	    }
   });
   $("#sidenavid").click(function(){
-    $("#navmenu",this).toggleClass("display-none")
+    	$("#navmenu",this).toggleClass("display-none")
   });
-  function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-	}
 
-  function closeNav() {
-	  document.getElementById("mySidenav").style.width = "0";
-   }
+   var index = 1;
+   var strA;
    setInterval(function(){
-   $('#myList li:nth-child(odd)').toggleClass('hideShowCase')}
+	   	switch(index) {
+		  case 0:
+		    strA = $(".img-choose-ul > li:eq(0)").html();index++;
+		    break;
+		  case 1:
+		    strA = $(".img-choose-ul > li:eq(1)").html();index++;
+		    break;
+		  case 2:
+		    strA = $(".img-choose-ul > li:eq(2)").html();index++;
+		    break;
+		  case 3:
+		    strA = $(".img-choose-ul > li:eq(3)").html();index++;
+		    break;
+		}
+		if(index == 4)
+			index = 0;
+	 	//alert(strA.toString());
+	   $('#myList li').html(strA)}
    ,3000);
- 
+
+ 	$(".slide").mouseenter(function(){
+	    //alert($(this).html());
+	    $('#myList li').html($(this).html())
+  });
 });
